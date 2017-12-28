cf82f4a2eb6a26252734412fd18c3778f061baf9
ueslem20 <ueslem71@gmail.com>
2017-12-28 10:37:05 -0200

    Exemplo Menu Nativo
    Desenvolvido com Thiago Cury

---
+++ ExMenuNativo/app/src/main/java/devcomcury/tk/exmenunativo/MainActivity.java
@@ -0,0 +1,42 @@
+package devcomcury.tk.exmenunativo;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.menu_tela1:
+                Intent it = new Intent(MainActivity.this,Tela1.class);
+                startActivity(it);
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
